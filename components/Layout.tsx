6b0b16ce4cf6c9b1038ae5499d87503d195166dd
Eirik Schøien <eirischo@stud.ntnu.no>
2022-02-06 19:29:12 +0100

    Tailwind modifications

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <div className='w-full'>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
