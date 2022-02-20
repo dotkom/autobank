dafa6c66f1ffe443594617ac39c21e7d1426b3f8
Anders Robstad <anderobs@stud.ntnu.no>
2022-02-20 18:23:19 +0100

    add page for receipt form

---
+++ pages/kvittering/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ExtraInformation from "../../components/forms/receipt/ExtraInformation";
+import ReciptInformation from "../../components/forms/receipt/ReciptInformation";
+import UserSection from "../../components/forms/receipt/UserSection";
+import Layout from "../../components/Layout/index";
+
+export default function ReciptForm() {
+  const [step, setStep] = useState<0 | 1 | 2>(0); //TODO: Add "enum" for steps, remove use of magic numbers.
+
+  const changeStep = (delta) => {
+    setStep(step + delta);
+  };
+
+  const submitForm = () => {
+    console.log("Submitting form");
+  };
+
+  return (
+    <Layout title="Kvittering">
+      <div className="max-w-lg w-full text-center">
+        <h1>Kvitteringsskjema</h1>
+        {step === 0 && <UserSection changeStep={changeStep} />}
+        {step === 1 && <ReciptInformation changeStep={changeStep} />}
+        {step === 2 && <ExtraInformation changeStep={changeStep} />}
+      </div>
+    </Layout>
+  );
+}
