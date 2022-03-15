a3f2be913b44a0df8d8b57e78af4a94217b15647
Eirik Schøien <eschoien@gmail.com>
2022-03-15 21:29:04 +0100

    Improve chatbot dialogue and appearance

--- components/Bankbot.tsx
+++ components/Bankbot.tsx
@@ -1,13 +1,31 @@
+import Link from 'next/link'
+import { useState } from 'react'
 import Chatbot from 'react-best-chatbot'
 
-/* 
-Types based on props at the bottom of this page.
-https://github.com/lucasfernando06/React-best-chatbot/blob/main/dist/index.js
-*/
+// https://www.npmjs.com/package/react-best-chatbot
+
+/**
+ * Creates a chatbot end step with blue link styling.
+ * @param url Url of destination in chatbot end step
+ * @param name Name of destination in chatbot end step
+ * @returns Chatbot end step with link styling
+ */
+const linkEndStep = (url: string, name: string) => ({
+  id: name,
+  content: (
+    <p>
+      Gå til{' '}
+      <span className="text-blue-500">
+        <Link href={'/' + url}>{name}</Link>
+      </span>
+    </p>
+  ),
+  end: true,
+})
 
 type Step = {
   id: number | string
-  content?: string
+  content?: string //| object
   receiveInput?: boolean
   delay?: number
   fetch?: Function
@@ -19,75 +37,73 @@ type Step = {
 }
 
 type Option = {
-  content?: string | object
+  content?: string //| Element
   value: number
   goTo: number | string
 }
 
 const steps: Step[] = [
   {
-    id: 'default',
+    id: 'start',
     content: 'Hei Onliner! Hvordan kan jeg hjelpe deg i dag?',
     options: [
-      { value: 10, content: 'Skal bruke penger', goTo: 'skal_bruke' },
-      { value: 20, content: 'Har allerede brukt penger', goTo: 'har_brukt' },
+      { value: 10, content: 'Har allerede brukt penger', goTo: 'har_brukt' },
+      { value: 20, content: 'Ønsker å bruke penger', goTo: 'vil_bruke' },
+      { value: 100, content: 'Ingen av disse passer', goTo: 'spor_bankom' },
     ],
   },
   {
-    id: 'skal_bruke',
-    content: 'Velg en av disse.',
+    id: 'har_brukt',
+    content: 'Velg en av disse:',
     options: [
       {
         value: 30,
+        content:
+          'Har bestilt en tjeneste/vare som Online skal bli fakturert for',
+        goTo: 'faktura',
+      },
+      {
+        value: 40,
+        content: 'Har brukt penger på vegne av Online',
+        goTo: 'kvittering',
+      },
+      { value: 100, content: 'Ingen av disse passer', goTo: 'spor_bankom' },
+    ],
+  },
+  {
+    id: 'vil_bruke',
+    content: 'Velg en av disse:',
+    options: [
+      {
+        value: 50,
         content:
           'Trenger over 10.000kr for å bruke noe som hjelper enhver Onliner',
         goTo: 'fondet',
       },
       {
-        value: 40,
+        value: 60,
         content:
           'Ønsker penger til motivasjon for å jobbe med Online-relatert arbeid',
         goTo: 'onlinepotten',
       },
+      { value: 100, content: 'Ingen av disse passer', goTo: 'spor_bankom' },
     ],
   },
   {
-    id: 'har_brukt',
-    content: 'Velg en av disse.',
-    options: [
-      {
-        value: 50,
-        content: 'Har brukt penger på vegne av online',
-        goTo: 'kvittering',
-      },
-      {
-        value: 60,
-        content:
-          'Har bestilt en tjeneste/vare som online skal bli fakturert for',
-        goTo: 'faktura',
-      },
-    ],
-  },
-  {
-    id: 'fondet',
-    content: 'Søk Online-fondet',
+    id: 'spor_bankom',
+    content: 'Send en mail til Bankom.',
     end: true,
   },
-  {
-    id: 'onlinepotten',
-    content: 'Søk Onlinepotten!',
-    end: true,
-  },
-  {
-    id: 'kvittering',
-    content: 'Fyll inn et kvitteringsskjema',
-    end: true,
-  },
-  {
-    id: 'faktura',
-    content: 'Gå til Online sin fakturainfo',
-    end: true,
-  },
+  /* Replacing the net four steeps below, but gives console error.
+  linkEndStep('fondet', 'Online-fondet'),
+  linkEndStep('onlinepotten', 'Onlinepotten'),
+  linkEndStep('kvittering', 'kvitteringsskjema'),
+  linkEndStep('faktura', 'fakturaskjema'),
+  */
+  { id: 'fondet', content: 'Søk Online-fondet!', end: true },
+  { id: 'onlinepotten', content: 'Søk Onlinepotten!', end: true },
+  { id: 'kvittering', content: 'Fyll ut kvitteringsskjema.', end: true },
+  { id: 'faktura', content: 'Fyll ut fakturaskjema.', end: true },
 ]
 
 const Bankbot = () => (
@@ -97,7 +113,14 @@ const Bankbot = () => (
       secondaryColor: '#F9B759',
       textColor: 'white',
     }}
-    options={{ header: 'Bankbot' }}
+    options={{
+      header: 'Bankbot',
+      sendMessage: 'Velg et alternativ...',
+      endContent: 'Start på nytt eller lukk oppe til høyre',
+      sendComponent: <p></p>, //removes 'send' icon
+      // botAvatarSrc: '', TODO: add custom chatbot avatar
+      // open: false, connect to link on indexpage
+    }}
     steps={steps}
   />
 )
