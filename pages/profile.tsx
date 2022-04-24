1e712e11e300e9f1c46a6d03b271e2ffbe1ee80a
sondrsc <sondrsc@stud.ntnu.no>
2022-04-24 18:30:45 +0200

    fixed routing for profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function profile() {
+  return (
+    <Layout>
+      <div>profile page</div>
+    </Layout>
+  )
+}
+
+export default profile
