8b5833707c513327f970499dab39a120389fa03c
sondrsc <sondrsc@stud.ntnu.no>
2022-07-08 18:49:15 +0200

    removed some bugs

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,16 +3,23 @@ import React from 'react'
 import Button from '../components/elements/Button'
 import Layout from '../components/Layout'
 
+import ProfileForm from './profile/ProfileForm'
+
 const Profile = () => {
   const { data: session, status } = useSession()
   return (
     <Layout>
-      <div>
+      <h2> Profile page</h2>
+      <ProfileForm />
+
+      {/* 
+         <div>
         {JSON.stringify(session)}, {status}
       </div>
       <Button pri={'primary'} onClick={() => signOut()}>
         Log ut
       </Button>
+      */}
     </Layout>
   )
 }
---
+++ pages/profile/ProfileForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ProfileForm() {
+  return <div>Endre profilinformasjon</div>
+}
+
+export default ProfileForm
