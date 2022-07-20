a33068d7cc7611e273fa0c0f71747662be62bd0a
sondrsc <sondrsc@stud.ntnu.no>
2022-07-20 16:35:38 +0200

    tailwind configuration + initial profile page setup

--- pages/profile/ProfileForm.tsx
+++ pages/profile/ProfileForm.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 function ProfileForm() {
-  return <div>Endre profilinformasjon</div>
+  return (
+    <div className="text-xl border-black w-500 container mx-auto">
+      <form className="mx-10 space-y-6" action="#" method="POST">
+        <div>
+          <p className="text-2xl"> Endre profilinformasjon </p>
+          <label htmlFor="email" className="text-sm font-medium text-gray-700">
+            Email address
+          </label>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default ProfileForm
