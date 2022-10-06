535c34d92f04deb077f0a52afb2fd12b42ad369e
JohanAOstbye <johan@ostbye.dev>
2022-10-06 17:32:20 +1100

    added the createContext

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { z } from 'zod'
+import { createContext } from '~/server/context'
 import { appRouter } from '~/server/routers/_app'
 
 // export type definition of API
@@ -9,5 +10,5 @@ export type AppRouter = typeof appRouter
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => null,
+  createContext,
 })
